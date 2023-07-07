56bac7400eeb088840181fdc8c62929022bd2368
AppleMax1992 <915803745@qq.com>
2023-07-07 10:04:17 +0800

    delete no use code

--- commitfit/exporters/openvino.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:6b9d2facc2ad4d5a1be7cea38e4e6c50c9bf9de2697fef56f3ef249c0b378c0a
-size 1719
--- commitfit/exporters/utils.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:e805cfe7eae0a412fbc9ef3c1ea1b851a99db272ac7c5c2eb1dfa30f9a6ba448
-size 1167
