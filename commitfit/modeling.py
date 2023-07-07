eeb1b4cff83a058fbd75becea4e3638d158d661f
AppleMax1992 <915803745@qq.com>
2023-07-07 10:35:16 +0800

    fix None issue

--- commitfit/modeling.py
+++ commitfit/modeling.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bd3ec4427a26f8b342eb3f6efda8c0acc076f96828f89763a83784cd7f7d8119
-size 32523
+oid sha256:03a15e2cac7140b1492913ed0ac1db16c42f2f6a398ed826a80db7d3272a75e2
+size 32547
